57d0573ad2d62b6b05d45de4d613536a577d05ae
KaterSun <maplekater@hotmail.com>
2020-04-14 21:38:58 -0400

    adding c++

---
+++ Source/TheClassicOfMAndS/MainCharacter.h
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/TimelineComponent.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "MainCharacter.generated.h"
+
+UCLASS()
+class THECLASSICOFMANDS_API AMainCharacter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMainCharacter();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enums")
+		float StaminaDrainRate;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enums")
+		float StaminaRecoverRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Running")
+		float WalkingSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Running")
+		float RunningSpeed;
+
+	/* Player stats
+	*/
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Stats")
+		float MaxHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+		float Health;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Stats")
+		float MaxStamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+		float Stamina;
+
+	UPROPERTY(VisibleAnywhere)
+		float Timer;
+
+
+protected:
+	
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/** Set the player base movements */
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	
+	void DecrementStamina(float Amount);
+	void DecrementHealth(float Amount);
+	void Die();
+
+};
